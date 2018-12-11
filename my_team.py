30b6dde24f9cefee882dd58109a104ac36ceeaf1
Tsukinai <dylan.blanchard@mymail.champlain.edu>
2018-12-10 21:08:43 -0500

    basic code
    added basic code for agent. still needs a lot of work to make function

--- my_team.py
+++ my_team.py
@@ -23,7 +23,7 @@ The core projects and autograders were primarily created by John DeNero
 Student side autograding was added by Brad Miller, Nick Hay, and
 Pieter Abbeel (pabbeel@cs.berkeley.edu).
 """
-
+import util
 from capture_agents import CaptureAgent
 import random
 
@@ -34,7 +34,7 @@ import random
 
 
 def create_team(first_index, second_index, is_red,
-                first='DummyAgent', second='DummyAgent'):
+                first='OffenceAgent', second='DefenceAgent'):
     """Create a team with agent indices first_index and second_index.
 
     This function returns a list of two agents that will form the
@@ -59,7 +59,7 @@ def create_team(first_index, second_index, is_red,
 ##########
 
 
-class DummyAgent(CaptureAgent):
+class BasicAgent(CaptureAgent):
     """A Dummy agent to serve as an example of the necessary agent structure.
 
     You should look at baseline_team.py for more details about how to
@@ -81,6 +81,7 @@ class DummyAgent(CaptureAgent):
         # use Manhattan distances instead of maze distances in order to save
         # on initialization time, please take a look at
         # CaptureAgent.register_initial_state in capture_agents.py.
+        self.start = game_state.get_agent_position(self.index)
         CaptureAgent.register_initial_state(self, game_state)
 
         # Your initialization code goes here, if you need any.
@@ -92,3 +93,36 @@ class DummyAgent(CaptureAgent):
         # You should change this in your own agent.
 
         return random.choice(actions)
+
+    def get_successor(self, game_state, action):
+        return game_state.generate_successor(self.index, action)
+
+    def evaluate(self, game_state, action):
+        features = self.get_features(game_state, action)
+        weights = self.get_weights(game_state, action)
+        return features * weights
+
+    def get_features(self, game_state, action):
+        features = util.Counter()
+        successor = self.get_successor(game_state, action)
+        features['successor_score'] = self.get_score(successor)
+        return None
+
+    def get_weights(self, game_state, action):
+        return {'successor_score': 1.0}
+
+
+class OffenceAgent(BasicAgent):
+    def get_features(self, game_state, action):
+        super().get_features(game_state, action)
+
+    def get_weights(self, game_state, action):
+        super().get_weights(game_state, action)
+
+
+class DefenceAgent(BasicAgent):
+    def get_features(self, game_state, action):
+        super().get_features(game_state, action)
+
+    def get_weights(self, game_state, action):
+        super().get_weights(game_state, action)
